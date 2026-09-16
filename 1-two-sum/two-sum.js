f2f6e5bb0c95c24081bf5fcde2ab88e1dcb6f0ad
Subhransu parida G <subhransuparida35@gmail.com>
2026-09-16 22:49:59 +0530

    Time: 32 ms (36.58%) | Memory: 54.5 MB (65.16%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,15 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-//  hashmap it reduce the time complexity
 var twoSum = function(nums, target) {
-    const map = new Map();
-
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i];
-        if (map.has(diff)){
-            return [map.get(diff), i ]
+  for (i=0 ; i< nums.length;i++){
+    for (let j=i+1 ; j<nums.length; j++){
+        if(nums[i] + nums[j]===target){
+            return[i,j]
         }
-        map.set(nums[i], i)
     }
+  }
+
 };
